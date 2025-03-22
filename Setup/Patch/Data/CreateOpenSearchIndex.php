0ceedfd005487d10bfd95fada4fc6b4e343fa60a
Jakub Winkler <jwinkler@magearchitect.com>
2025-03-22 12:23:09 +0100

    v1.0.2 - module update, support for qoliber/core

--- Setup/Patch/Data/CreateOpenSearchIndex.php
+++ Setup/Patch/Data/CreateOpenSearchIndex.php
@@ -27,7 +27,6 @@ class CreateOpenSearchIndex implements DataPatchInterface
         private readonly ScopeConfigInterface $scopeConfig,
         private readonly ClientResolver $clientResolver,
     ) {
-
     }
 
     /**
@@ -38,85 +37,85 @@ class CreateOpenSearchIndex implements DataPatchInterface
     public function apply(): CreateOpenSearchIndex
     {
         /** @var \Magento\OpenSearch\Model\OpenSearch $client */
-        if ($client = $this->clientResolver->create()) {
-            if ($client->getOpenSearchClient()->indices()->exists(
-                ['index' => $this->scopeConfig->getValue('qoliber_psl/opensearch/index')]
-            )) {
-                return $this;
-            }
+        $client = $this->clientResolver->create();
 
-            $client->getOpenSearchClient()->indices()->create([
-                'index' => $this->scopeConfig->getValue('qoliber_psl/opensearch/index'),
-                'body' => [
-                    'settings' => [
-                        "index.max_ngram_diff" => 10,
-                        'analysis' => [
-                            'tokenizer' => [
-                                'ngram_tokenizer' => [
-                                    'type' => 'ngram',
-                                    'min_gram' => 2,
-                                    'max_gram' => 10,
-                                    'token_chars' => ['letter', 'digit']
-                                ]
+        if ($client->getOpenSearchClient()->indices()->exists(
+            ['index' => $this->scopeConfig->getValue('qoliber_psl/opensearch/index')]
+        )) {
+            return $this;
+        }
+
+        $client->getOpenSearchClient()->indices()->create([
+            'index' => $this->scopeConfig->getValue('qoliber_psl/opensearch/index'),
+            'body' => [
+                'settings' => [
+                    "index.max_ngram_diff" => 10,
+                    'analysis' => [
+                        'tokenizer' => [
+                            'ngram_tokenizer' => [
+                                'type' => 'ngram',
+                                'min_gram' => 2,
+                                'max_gram' => 10,
+                                'token_chars' => ['letter', 'digit']
+                            ]
+                        ],
+                        'analyzer' => [
+                            'ngram_analyzer' => [
+                                'type' => 'custom',
+                                'tokenizer' => 'ngram_tokenizer',
+                                'filter' => ['lowercase', 'asciifolding']
                             ],
-                            'analyzer' => [
-                                'ngram_analyzer' => [
-                                    'type' => 'custom',
-                                    'tokenizer' => 'ngram_tokenizer',
-                                    'filter' => ['lowercase', 'asciifolding']
-                                ],
-                                'value_normalizer' => [
-                                    'type' => 'custom',
-                                    'tokenizer' => 'standard',
-                                    'filter' => ['lowercase', 'asciifolding']
-                                ]
+                            'value_normalizer' => [
+                                'type' => 'custom',
+                                'tokenizer' => 'standard',
+                                'filter' => ['lowercase', 'asciifolding']
                             ]
                         ]
-                    ],
-                    'mappings' => [
-                        'properties' => [
-                            'customer_id' => ['type' => 'integer'],
-                            'status' => ['type' => 'integer'],
-                            'account_type' => ['type' => 'keyword'],
-                            'company_name' => ['type' => 'text', 'index' => false],
-                            'logo' => ['type' => 'keyword', 'index' => false],
-                            'homepage_url' => ['type' => 'keyword', 'index' => false],
-                            'full_address' => ['type' => 'text', 'index' => false],
-                            'location' => [
-                                'type' => 'geo_point'
-                            ],
-                            'short_description' => ['type' => 'text', 'index' => false],
-                            'description' => ['type' => 'text', 'index' => false],
+                    ]
+                ],
+                'mappings' => [
+                    'properties' => [
+                        'customer_id' => ['type' => 'integer'],
+                        'status' => ['type' => 'integer'],
+                        'account_type' => ['type' => 'keyword'],
+                        'company_name' => ['type' => 'text', 'index' => false],
+                        'logo' => ['type' => 'keyword', 'index' => false],
+                        'homepage_url' => ['type' => 'keyword', 'index' => false],
+                        'full_address' => ['type' => 'text', 'index' => false],
+                        'location' => [
+                            'type' => 'geo_point'
+                        ],
+                        'short_description' => ['type' => 'text', 'index' => false],
+                        'description' => ['type' => 'text', 'index' => false],
 
-                            'country' => [
-                                'type' => 'text',
-                                'analyzer' => 'value_normalizer'
-                            ],
-                            'city' => [
-                                'type' => 'text',
-                                'analyzer' => 'ngram_analyzer',
-                                'search_analyzer' => 'value_normalizer'
-                            ],
-                            'services' => ['type' => 'keyword'],
-                            'certificates' => [
-                                'type' => 'nested',
-                                'properties' => [
-                                    'certificate_type' => ['type' => 'keyword'],
-                                    'count' => ['type' => 'integer']
-                                ]
-                            ],
-                            'social_media_links' => [
-                                'type' => 'nested',
-                                'properties' => [
-                                    'name' => ['type' => 'keyword'],
-                                    'url' => ['type' => 'keyword']
-                                ]
+                        'country' => [
+                            'type' => 'text',
+                            'analyzer' => 'value_normalizer'
+                        ],
+                        'city' => [
+                            'type' => 'text',
+                            'analyzer' => 'ngram_analyzer',
+                            'search_analyzer' => 'value_normalizer'
+                        ],
+                        'services' => ['type' => 'keyword'],
+                        'certificates' => [
+                            'type' => 'nested',
+                            'properties' => [
+                                'certificate_type' => ['type' => 'keyword'],
+                                'count' => ['type' => 'integer']
+                            ]
+                        ],
+                        'social_media_links' => [
+                            'type' => 'nested',
+                            'properties' => [
+                                'name' => ['type' => 'keyword'],
+                                'url' => ['type' => 'keyword']
                             ]
                         ]
                     ]
                 ]
-            ]);
-        }
+            ]
+        ]);
 
         return $this;
     }
